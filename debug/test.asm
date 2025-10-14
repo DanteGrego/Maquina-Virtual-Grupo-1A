db08bfbdcf7adebd23b368c91be5e8fd33599e4c
Santiago <santiagomartineznh@gmail.com>
2025-10-14 14:40:34 -0300

    dissasembler

--- debug/test.asm
+++ debug/test.asm
@@ -1,8 +1,12 @@
 main:   mov eax, 10
         mov ecx, 10
         add eax, ecx
-        mov [ds], eax
+        mov W[ds], eax
         mov eax, 1
+        ret
+        push al
+        pop ax
+        call main
         ldl ecx, 1
         ldh ecx, 4
         sys 2
