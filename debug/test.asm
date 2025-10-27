a84b5c7dd48796d789d4ebccb8c3f5daa9c6bb29
IanaiGribman <ianaigribman@gmail.com>
2025-10-27 16:43:47 -0300

    cambios pequeños en task.json

--- debug/test.asm
+++ debug/test.asm
@@ -1,20 +1,39 @@
 main:   push bp
         mov bp, sp
-        push ecx
-        push edx
-        sys 0xf
-        sub sp, 8
-        mov edx, BP
-        sub edx, 8
-        mov cx, 8
-        sys 0xf
-        sys 0xf
-        sys 3
-        sys 0xf
-        sys 4
-        pop edx
-        pop ecx
-        sys 0xf
+
+        mov edx, ds
+        mov eax, 1
+        mov cx, 1
+        ldh ecx, 4
+
+        mov efx, 1
+        mov [edx], efx
+        sys 2
+
+        mov efx, 0
+        mov fl, 1
+        mov [edx], efx
+        sys 2
+
+        mov efx, -1
+        mov [edx], efx
+        sys 2
+
+        mov efx, 0
+        mov fh, 1
+        mov [edx], efx
+        sys 2
+
+        mov efx, -1
+        mov [edx], 0
+        mov b[edx], efx
+        sys 2
+
+        mov efx, -1
+        mov [edx], 0
+        mov w[edx], efx
+        sys 2
+        
         mov sp, bp
         pop bp
         ret
